57b413a99c1e1afb1b0248c14d53826f2c5fdfac
JC <22549889@sun.ac.za>
2022-01-27 06:19:21 +0200

    fixed restart audio glitch

--- commands/restart.js
+++ commands/restart.js
@@ -6,7 +6,8 @@ module.exports = {
 		.setDescription('Restarts song'),
 	async execute(interaction) {
 		const player=interaction.client.queue.get(interaction.guildId)	
-		playing();
+		player.queue.unshift(player.queue[0])
+		player.player.stop();
 		return interaction.reply("Restarting song");
 	},
 };
